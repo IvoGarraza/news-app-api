cc3da022134bf73e9d76fde8a17df66bad781c1f
IvoGarraza <ivogarrachip@gmail.com>
2025-07-06 23:26:02 -0300

    Eliminar archivo de configuración sensible y agregarlo al .gitignore

--- app-news-api/Program.cs
+++ app-news-api/Program.cs
@@ -12,6 +12,18 @@ var connectionString = builder.Configuration.GetSection("PostgreSQL:ConnectionSt
 builder.Services.AddSingleton(new PostgreSQLConfiguration(connectionString));
 builder.Services.AddScoped<app_news_api.Data.Repositories.INewsRepository, app_news_api.Data.Repositories.NewsRepository>();
 
+//Configuracion de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,5 +40,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowReactApp");
 
 app.Run();
